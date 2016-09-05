f66160e9fc57c58d0d901d70faf45163c88b476d
Quang Vo <duyquang1202@gmail.com>
2016-09-06 02:03:55 +0700

    permission

--- vagrant/docker/phpfpm/start.sh
+++ vagrant/docker/phpfpm/start.sh
@@ -6,6 +6,10 @@ mkdir -p /var/lib/php/session
 chmod -R 777 /var/lib/php/session
 chmod -R 777 /var/www/pon/var/cache
 chmod -R 777 /var/www/pon/var/logs
+chmod -R 777 /var/www/pon/var/node_modules
+chmod -R 777 /var/www/pon/var/bower_components
+setfacl -R -m u:www-data:rwx -m u:`whoami`:rwx /var/www/pon/var/cache /var/www/pon/var/logs /var/www/pon/var/node_modules /var/www/pon/var/bower_components
+setfacl -dR -m u:www-data:rwx -m u:`whoami`:rwx /var/www/pon/var/cache /var/www/pon/var/logs /var/www/pon/var/node_modules /var/www/pon/var/bower_components
 
 /usr/local/sbin/php-fpm
 
