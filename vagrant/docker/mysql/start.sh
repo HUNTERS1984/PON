80395b72f55cf521d9c86257b3b249bf579afb52
Quang Vo <duyquang1202@gmail.com>
2016-09-06 02:21:14 +0700

    permission

--- vagrant/docker/mysql/start.sh
+++ vagrant/docker/mysql/start.sh
@@ -3,11 +3,10 @@
 # if the /data/mariadb directory doesn't contain anything, then initialise it
 directory="/data/mariadb/data"
 
+chmod -R 777 /data/mariadb
+setfacl -R -m u:www-data:rwx -m u:`whoami`:rwx /data/mariadb
+setfacl -dR -m u:www-data:rwx -m u:`whoami`:rwx /data/mariadb
 if [ ! "$(ls -A $directory)" ]; then
-     chmod -R 777 /data/mariadb/data
-     setfacl -R -m u:www-data:rwx -m u:`whoami`:rwx /data/mariadb/data
-     setfacl -dR -m u:www-data:rwx -m u:`whoami`:rwx /data/mariadb/data
-
     /usr/bin/mysql_install_db --datadir=/data/mariadb/data --user=mysql
     /usr/bin/mysqld_safe --init-file=/opt/bin/mariadb-setup.sql
 else
