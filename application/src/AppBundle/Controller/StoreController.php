373aa0b4cfc06d05e867e65a4625afddc3b34e04
Quang Vo <duyquang1202@gmail.com>
2016-09-29 08:43:36 +0700

    shop coupons by map

--- application/src/AppBundle/Controller/StoreController.php
+++ application/src/AppBundle/Controller/StoreController.php
@@ -325,6 +325,129 @@ class StoreController extends FOSRestController  implements ClassResourceInterfa
     }
 
     /**
+     * Get Shop Coupons By Map
+     * @ApiDoc(
+     *  resource=true,
+     *  description="This api is used to list coupon",
+     *  requirements={
+     *      {
+     *          "name"="longt",
+     *          "dataType"="integer",
+     *          "description"="type (1,2,3,4)"
+     *      }
+     *  },
+     *  headers={
+     *         {
+     *             "name"="Authorization",
+     *             "description"="Bearer [token key]"
+     *         }
+     *     },
+     *  parameters={
+     *      {"name"="page_size", "dataType"="integer", "required"=false, "description"="page size to return"},
+     *      {"name"="page_index", "dataType"="integer", "required"=false, "description"="page index to return"},
+     *  },
+     *  statusCodes = {
+     *     200 = "Returned when successful",
+     *     401="Returned when the user is not authorized"
+     *   }
+     * )
+     * @Get("/map/{lattitude}/{longitude}/shops")
+     * @return Response
+     */
+    public function getShopCouponByMapAction($lattitude, $longitude, Request $request)
+    {
+        $faker = Factory::create('ja_JP');
+        $data = [];
+        $j = 0;
+        for ($i = 0; $i < 20; $i++) {
+            $data[] = [
+                'id' => $i+1,
+                'title' => $faker->company,
+                'operation_start_time' =>  '08:00',
+                'operation_end_time' => '23:00',
+                'avatar_url' => $faker->imageUrl(640, 480, 'food'),
+                'is_follow' => $faker->randomElement([0, 1]),
+                'tel' => $faker->phoneNumber,
+                'lattitude' => $lattitude,
+                'longitude' => $longitude,
+                'address' => $faker->address,
+                'close_date' => "Saturday and Sunday",
+                'ave_bill' => $faker->numberBetween(100, 200),
+                'help_text' => $faker->paragraph(2),
+                'shop_photo_url' => [
+                    $faker->imageUrl(640, 480, 'food'),
+                    $faker->imageUrl(640, 480, 'food'),
+                    $faker->imageUrl(640, 480, 'food'),
+                    $faker->imageUrl(640, 480, 'food')
+                ],
+                'coupons' => [
+                    [
+                        'id' => 1,
+                        'title' => $faker->name,
+                        'image_url' => $faker->imageUrl(640, 480, 'food'),
+                        'expired_time' => new \DateTime(),
+                        'is_like' => $faker->randomElement([0, 1]),
+                        'can_use' => $faker->randomElement([0, 1]),
+                        'coupon_type' => [
+                            'id' => $faker->randomElement([0, 1]),
+                            'name' => $faker->name,
+                            'icon_url' => $faker->imageUrl(640, 480, 'food')
+                        ],
+                    ],
+                    [
+                        'id' => 2,
+                        'title' => $faker->name,
+                        'image_url' => $faker->imageUrl(640, 480, 'food'),
+                        'expired_time' => new \DateTime(),
+                        'is_like' => $faker->randomElement([0, 1]),
+                        'can_use' => $faker->randomElement([0, 1]),
+                        'coupon_type' => [
+                            'id' => $faker->randomElement([0, 1]),
+                            'name' => $faker->name,
+                            'icon_url' => $faker->imageUrl(640, 480, 'food')
+                        ],
+                    ],
+                    [
+                        'id' => 3,
+                        'title' => $faker->name,
+                        'image_url' => $faker->imageUrl(640, 480, 'food'),
+                        'expired_time' => new \DateTime(),
+                        'is_like' => $faker->randomElement([0, 1]),
+                        'can_use' => $faker->randomElement([0, 1]),
+                        'coupon_type' => [
+                            'id' => $faker->randomElement([0, 1]),
+                            'name' => $faker->name,
+                            'icon_url' => $faker->imageUrl(640, 480, 'food')
+                        ],
+                    ],
+                    [
+                        'id' => 4,
+                        'title' => $faker->name,
+                        'image_url' => $faker->imageUrl(640, 480, 'food'),
+                        'expired_time' => new \DateTime(),
+                        'is_like' => $faker->randomElement([0, 1]),
+                        'can_use' => $faker->randomElement([0, 1]),
+                        'coupon_type' => [
+                            'id' => $faker->randomElement([0, 1]),
+                            'name' => $faker->name,
+                            'icon_url' => $faker->imageUrl(640, 480, 'food')
+                        ],
+                    ],
+                ]
+            ];
+            $j = $j + 5;
+        }
+        return $this->view(BaseResponse::getData($data, [
+            'limit' => 20,
+            'offset' => 0,
+            'item_total' => 20,
+            'page_total' => 1,
+            'current_page' => 1
+        ]));
+
+    }
+
+    /**
      * @return StoreManager
      */
     public function getManager()
