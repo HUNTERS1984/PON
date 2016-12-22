113a8a34eec57a2a9c50054cf4e2a0fb1e97fd47
Quang Vo <duyquang1202@gmail.com>
2016-12-22 16:57:35 +0700

    update use list

--- application/src/CoreBundle/Queue/Consumer/SyncPostConsumer.php
+++ application/src/CoreBundle/Queue/Consumer/SyncPostConsumer.php
@@ -55,8 +55,6 @@ class SyncPostConsumer implements ConsumerInterface
             $this->logger->error(sprintf("Sync Post Job Was Failed With ID: %s", $post->getId()));
             $this->logger->error($e->getMessage());
         }
-
-        die();
     }
 
     public function process(Post $post)
