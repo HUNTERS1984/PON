2ba0973ac0c3381b752306e76d8e771d6cef93af
Quang Vo <duyquang1202@gmail.com>
2016-12-25 22:21:27 +0700

    forgot password

--- application/src/AdminBundle/Controller/SecurityController.php
+++ application/src/AdminBundle/Controller/SecurityController.php
@@ -3,7 +3,12 @@
 namespace AdminBundle\Controller;
 
 use CoreBundle\Entity\AppUser;
+use CoreBundle\Manager\AppUserManager;
+use CoreBundle\Utils\StringGenerator;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends Controller
@@ -30,13 +35,135 @@ class SecurityController extends Controller
         );
     }
 
-    public function forgotAction()
+    public function forgotAction(Request $request)
     {
-        return $this->render('AdminBundle:Security:forgot.html.twig');
+        $form = $this->createFormBuilder()
+            ->add('email', EmailType::class, [
+                'label' => 'Eメールアドレス',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Eメールアドレス',
+                    'autocomplete' => 'off',
+                ]
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $email = $data['email'];
+            /**@var AppUser $appUser */
+            $appUser = $this->getManager()->getAppUserByEmail($email);
+            if(!$appUser) {
+                $this->addFlash('forgot_password_error', "電子メールが見つかりませんでした");
+            }else{
+                $this->resetPassword($appUser);
+                return $this->render('AdminBundle:Security:forgot_success.html.twig');
+            }
+
+        }
+        return $this->render('AdminBundle:Security:forgot.html.twig',[
+            'form' => $form->createView(),
+        ]);
+    }
+
+    public function resetAction($id, Request $request)
+    {
+        $token = $request->get('token');
+        $appUser = $this->getManager()->getAppUserById($id);
+        if(!$appUser) {
+            throw $this->createNotFoundException('ユーザーは見つかりませんでした。');
+        }
+
+        if($appUser->getResetToken() != $token || $appUser->getTokenExpired() < new \DateTime()) {
+            throw $this->createNotFoundException('トークンが失効しました');
+        }
+
+        $form = $this->createFormBuilder()
+            ->add('password', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'invalid_message' => 'パスワードフィールドは一致する必要があります。',
+                'options' => array('attr' => array('class' => 'form-control')),
+                'required' => true,
+                'first_options'  => [
+                    'label' => 'パスワード',
+                    'attr' => [
+                        'class' => 'form-control',
+                        'placeholder' => 'パスワード'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'パスワードを認証する',
+                    'attr' => [
+                        'class' => 'form-control',
+                        'placeholder' => 'パスワードを認証する'
+                    ]
+                ],
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // perform some action...
+            $data = $form->getData();
+            $appUser
+                ->setResetToken(null)
+                ->setTokenExpired(null)
+                ->setPlainPassword($data['password']);
+
+            $appUser = $this->getManager()->saveAppUser($appUser);
+            if($appUser) {
+                return $this->render('AdminBundle:Security:success.html.twig',[
+                ]);
+            }else{
+                return $this->render('AdminBundle:Security:false.html.twig',[
+                ]);
+            }
+
+        }
+
+
+        return $this->render('AdminBundle:Security:reset.html.twig',[
+            'token' => $token,
+            'id' => $id,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    public function resetPassword(AppUser $appUser)
+    {
+        $expiredTime = new \DateTime();
+        $expiredTime->modify("+24 hours");
+        $appUser
+            ->setTokenExpired($expiredTime)
+            ->setResetToken(StringGenerator::secureGenerate());
+        $subject = "[PON]パスワードリセット通知";
+        $body = $this->get('twig')->render(
+            'AdminBundle:Security:forgot_email.html.twig',
+            [
+                'appUser' => $appUser
+            ]
+        );
+        $sender = $this->getParameter('mailer_sender');
+        $senderName = $this->getParameter('mailer_sender_name');
+        $this->getManager()->saveAppUser($appUser);
+        $this->getManager()->sendForGotPasswordEmail($appUser, $subject, $body, $sender, $senderName);
+
     }
 
     public function logoutAction()
     {
         return $this->redirectToRoute('admin_login');
+    }
+
+    /**
+     * @return AppUserManager
+    */
+    public function getManager()
+    {
+        return $this->get('pon.manager.app_user');
     }
 }
