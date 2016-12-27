bf9b6d0d4ec244f6c7d08483cea04da674893996
Quang Vo <duyquang1202@gmail.com>
2016-12-27 22:06:13 +0700

    setting link

---
+++ application/src/CustomerBundle/Controller/SettingController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace CustomerBundle\Controller;
+
+use CoreBundle\Manager\SettingManager;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SettingController extends Controller
+{
+    public function contactAction()
+    {
+        $setting = $this->getManager()->getSetting('contact');
+        if (!$setting) {
+           throw $this->createNotFoundException("連絡先が見つかりませんでした...");
+        }
+
+        return $this->render('CustomerBundle:Setting:contact.html.twig',[
+            'setting' => $setting
+        ]);
+    }
+
+    public function privacyAction()
+    {
+        $setting = $this->getManager()->getSetting('privacy');
+        if (!$setting) {
+            throw $this->createNotFoundException("連絡先が見つかりませんでした...");
+        }
+
+        return $this->render('CustomerBundle:Setting:privacy.html.twig',[
+            'setting' => $setting
+        ]);
+    }
+
+    public function termAction()
+    {
+        $setting = $this->getManager()->getSetting('term');
+        if (!$setting) {
+            throw $this->createNotFoundException("連絡先が見つかりませんでした...");
+        }
+
+        return $this->render('CustomerBundle:Setting:term.html.twig',[
+            'setting' => $setting
+        ]);
+    }
+
+    public function tradeAction()
+    {
+        $setting = $this->getManager()->getSetting('trade');
+        if (!$setting) {
+            throw $this->createNotFoundException("連絡先が見つかりませんでした...");
+        }
+
+        return $this->render('CustomerBundle:Setting:trade.html.twig',[
+            'setting' => $setting
+        ]);
+    }
+
+    /**
+     * @return SettingManager
+     */
+    public function getManager()
+    {
+        return $this->get('pon.manager.setting');
+    }
+}
